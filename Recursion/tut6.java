072e203f6bc48d8a153e6d97c89b077956c479b7
Sameer Ojha <146799637+Sameer172003@users.noreply.github.com>
2024-06-23 14:18:40 +0530

    added

---
+++ Recursion/tut6.java
@@ -0,0 +1,42 @@
+// Permutation of an array
+
+import java.util.*;
+public class tut6 {
+    public static void swap(List<Integer> sl,int i,int j){
+        int temp=0;
+        temp=sl.get(i);
+        sl.set(i,sl.get(j));
+        sl.set(j,temp);
+    }
+    public static void helper(List<Integer> sl,List<List<Integer>> ans,int idx){
+        if(idx == sl.size()){
+            ans.add(new ArrayList<>(sl));
+            return;
+        }
+        for(int k=idx;k<sl.size();k++){
+            swap(sl, idx, k);
+            helper(sl, ans, idx+1);
+            swap(sl, idx, k);
+        }
+    }
+    public static List<List<Integer>> permutation(int arr[]){
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> sl=new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            sl.add(arr[i]);
+        }
+        helper(sl, ans, 0);
+        return ans;
+    }
+    public static void main(String args[]){
+        int arr[]={1,2,3};
+        List<List<Integer>> ans=new ArrayList<>();
+        ans.addAll(permutation(arr));
+        for(int i=0;i<ans.size();i++){
+            for(int j=0;j<ans.get(0).size();j++){
+                System.out.print(ans.get(i).get(j)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Recursion/tut7.java
@@ -0,0 +1,39 @@
+// Permutaion of string
+
+import java.util.*;
+public class tut7 {
+    public static String tempStr;
+    private static void swap(String s, int i, int j) {
+        StringBuilder sb = new StringBuilder(s);
+        sb.setCharAt(i, s.charAt(j));
+        sb.setCharAt(j, s.charAt(i));
+        tempStr = sb.toString();
+    }
+    private static void Helper(String s, int i, int j, List<String> ans) {
+        if (i == j) {
+            ans.add(s);
+            return;
+        }
+        for (int k = i; k <= j; k++) {
+            swap(s, i, k);
+			s=tempStr;
+            Helper(s, i + 1, j, ans);
+            swap(s, i, k);
+			s=tempStr;
+        }
+    }
+    public static List<String> generatePermutations(String s) {
+        List<String> ans = new ArrayList<>();
+        Helper(s, 0, s.length() - 1, ans);
+		Collections.sort(ans);
+        return ans;
+	}
+    public static void main(String args[]){
+        String s="abc";
+        List<String> ans = new ArrayList<>();
+        ans.addAll(generatePermutations(s));
+        for(int i=0;i<ans.size();i++){
+            System.out.print(ans.get(i)+" ");
+        }
+    }
+}
