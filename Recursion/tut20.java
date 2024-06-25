383c4e51f2e840620433a6d16b8b9e1190fc86b0
Sameer Ojha <146799637+Sameer172003@users.noreply.github.com>
2024-06-25 16:34:05 +0530

    added

--- Recursion/tut20.java
+++ Recursion/tut20.java
@@ -9,14 +9,11 @@ public class tut20 {
         nq(board, 0, res);
         return res;
     }
-    
-    
     static void nq(boolean[][] board, int row, ArrayList<ArrayList<Integer>> res) {
         if(row==board.length) {
             res.add(display(board));
             return;
         }
-        
         for(int col = 0; col<board.length; col++) {
             if(isSafe(board, row, col)) {
                 board[row][col] = true;
@@ -25,21 +22,18 @@ public class tut20 {
             }
         }
     }
-    
     static boolean isSafe(boolean[][] board, int row, int col) {
         for(int i = 0; i<row; i++) {
             if(board[i][col]) {
                 return false;
             }
         }
-        
         int maxLeft = Math.min(row, col);
         for(int i = 1; i<=maxLeft; i++) {
             if(board[row-i][col-i]) {
                 return false;
             }
         }
-        
         int maxRight = Math.min(row, board.length-col-1);
         for(int i = 1; i<=maxRight; i++) {
             if(board[row-i][col+i]) {
@@ -48,7 +42,6 @@ public class tut20 {
         }
         return true;
     }
-    
     static ArrayList<Integer> display(boolean[][] board) {
         ArrayList<Integer> sol = new ArrayList<>();
         
